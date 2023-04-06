a88641a97bac9937027a1b46b69d63dfe50a8168
“Sean <sean.she@berkeley.edu>
2023-04-05 19:04:41 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `berke1337.berkeley.edu`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
